fd5b51dd0af6dc2259a32c519b66619f90e1ed34
Anthony Shull <shull.anthony@gmail.com>
2023-12-23 19:57:11 -0600

    convert all keys to atoms

--- lib/open_api_spex_impl/client.ex
+++ lib/open_api_spex_impl/client.ex
@@ -19,8 +19,9 @@ defmodule OpenApiSpexImpl.Client do
       }) do
     case Tesla.request(@client, method: method, query: query, url: url) do
       {:ok, %Tesla.Env{body: body}} ->
-        map = Map.new(body, fn {k, v} -> {String.to_atom(k), v} end)
-        Kernel.struct(struct, map)
+        body
+        |> KeyConvert.atomize()
+        |> (&Kernel.struct(struct, &1)).()
 
       {:error, error} ->
         {:error, error}
